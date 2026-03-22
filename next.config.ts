38e0350164bec47a189a5d085e26af52332a8334
Aaroncheng564 <aaroncheng@example.com>
2026-03-22 20:24:03 +0800

    Add allowedDevOrigins for development

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    '07af79f3261654ba-111-229-154-141.serveousercontent.com',
+    'localhost:3001',
+    'localhost:3000',
+  ],
 };
 
 export default nextConfig;
